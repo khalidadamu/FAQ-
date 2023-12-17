c6928ef3a1457b8acec67a631ad479a382b85201
khalidadamu <khalidbox720@icloud.com>
2023-12-17 21:49:36 +0100

    fixed a bug when closing the accordion tap

--- src/accordion.js
+++ src/accordion.js
@@ -8,6 +8,8 @@ for (let i = 0; i < acc.length; i++) {
       currentlyOpen.classList.remove("active");
       let currentPanel = currentlyOpen.nextElementSibling;
       currentPanel.style.maxHeight = null;
+      // Reset the background color when closing
+      currentlyOpen.style.backgroundColor = "black";
     }
 
     // Toggle the clicked accordion
@@ -15,8 +17,12 @@ for (let i = 0; i < acc.length; i++) {
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
+      // Reset the background color when closing
+      this.style.backgroundColor = "black";
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+      // Change the background color when expanding
+      this.style.backgroundColor = "wheat"; // Change this to your desired color
     }
 
     // Update the currently open accordion
